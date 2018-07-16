c7d87b4be05f9c12872ece28886f2846dcf2b988
abecel22 <abe2211@gmail.com>
2018-07-15 19:29:06 -0500

    Finished second mortgage calculator form.

--- src/app/components/calculator/calculator.component.ts
+++ src/app/components/calculator/calculator.component.ts
@@ -10,22 +10,32 @@ import { FormControl } from '@angular/forms';
 })
 export class CalculatorComponent implements OnInit {
   calcForm: FormGroup;
+  calcForm2: FormGroup;
   loanAmount: number;
   interestRate: number;
+  interestRate2: number;
   numPayments: number;
+  numPayments2: number;
+  maxPayment: number;
   monthlyPayment: number = 1418;
   totalPaid: number = 510640;
+  maxLoan: number = 300000;
   constructor() {}
 
   ngOnInit() {
     this.calcForm = new FormGroup({
       loanAmount: new FormControl('300000'),
       interestRate: new FormControl('3.92'),
-      morgPeriod: new FormControl('30'),
-      monthlyPayment: new FormControl('1418')
+      morgPeriod: new FormControl('30')
+    });
+    this.calcForm2 = new FormGroup({
+      interestRate2: new FormControl('3.92'),
+      morgPeriod2: new FormControl('30'),
+      maxPayment: new FormControl('1418')
     });
 
     this.calcForm.valueChanges.subscribe((value) => this.onSubmit());
+    this.calcForm2.valueChanges.subscribe((value) => this.onSubmit2());
   }
 
   onSubmit() {
@@ -43,6 +53,14 @@ export class CalculatorComponent implements OnInit {
           ((1 + this.interestRate) ** this.numPayments - 1)) *
         this.numPayments
     );
-    console.log(this.monthlyPayment);
+  }
+  onSubmit2() {
+    this.interestRate2 = this.calcForm2.value.interestRate2 / 100 / 12;
+    this.numPayments2 = this.calcForm2.value.morgPeriod2 * 12;
+    this.maxPayment = this.calcForm2.value.maxPayment;
+    this.maxLoan = Math.round(
+      (this.maxPayment * ((1 + this.interestRate2) ** this.numPayments2 - 1)) /
+        (this.interestRate2 * (1 + this.interestRate2) ** this.numPayments2)
+    );
   }
 }
